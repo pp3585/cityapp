95c109435349fa3a793adf3e05c9d1bbfcb6ac96
pp3585 <pooja.prasa@gmail.com>
2017-01-30 07:27:02 -0500

    Hindi Detail pages done Padding for list view added TODO Retain tab state onResume make data call for current tab TODO if reqd Progress for list page Share option in webview ANalytics India map as default placeholder progress for detail page

--- app/src/main/java/com/samples/pooja/cityapp/webhandlers/NewsParserHtml.java
+++ app/src/main/java/com/samples/pooja/cityapp/webhandlers/NewsParserHtml.java
@@ -15,12 +15,61 @@ import java.io.IOException;
 /**
  * Html parser for News details
  */
-public class NewsParserHtml extends NewsParser {
+class NewsParserHtml extends NewsParser {
 
     //private String tempData = NewsPageConstants.TEMPDATA;
 
     @Override
     protected Object parse(String result) throws JSONException, XmlPullParserException, IOException {
+        NewsDetail newsDetail;
+        if(result.contains("timesofindia")){
+            newsDetail = parseToiData(result);
+        } else {
+            newsDetail = parseAmarUjalaData(result);
+        }
+        return newsDetail;
+    }
+
+    private NewsDetail parseAmarUjalaData(String result) {
+        NewsDetail newsDetail = new NewsDetail();
+        Document doc = Jsoup.parse(result);
+        String title = doc.select("div.ttl > h1").text();
+        if(title.equals("")){
+            title = doc.select("div.vdoarticle-desc > h1").text();
+        }
+        String source = doc.select("h4 > a").text();
+        String pubDate = doc.select("div.authDv > span").text();
+        if(pubDate.equals("")) {
+            pubDate = doc.select("div.desc > span").text();
+        }
+        String description = doc.select("div.desc > div").text();/* + " \n" + doc.select("div.desc:eq(1)").text();*/
+        if(description.equals("")){
+            description = doc.select("div.vdoarticle-desc > p").text();
+        }
+        if(description.equals("")){
+            description = doc.select("div.desc").text();
+        }
+        String imgSrc = doc.select("div.imgDv > img").attr("src");
+        if(!imgSrc.equals("")) {
+            imgSrc = "http:" + imgSrc;
+        }
+        String imgDesc = doc.select("div.imgDv > img").attr("alt");
+        newsDetail.setTitle(title);
+        newsDetail.setDescription(description);
+        newsDetail.setImgDesc(imgDesc);
+        if(imgSrc.equals("")){
+            newsDetail.setImgSrc(null);
+        } else {
+            //Below line is required to adjust size for mobile
+            imgSrc = imgSrc.replace("400", "1200");
+            newsDetail.setImgSrc(imgSrc);
+        }
+        newsDetail.setSource(source);
+        newsDetail.setPubDate(pubDate);
+        return newsDetail;
+    }
+
+    private NewsDetail parseToiData(String result){
         NewsDetail newsDetail = new NewsDetail();
         Document doc = Jsoup.parse(result);
         String title = doc.select("div.metatitle").attr("data-meta-title");
@@ -32,7 +81,7 @@ public class NewsParserHtml extends NewsParser {
         newsDetail.setTitle(title);
         newsDetail.setDescription(description);
         newsDetail.setImgDesc(imgDesc);
-        if(imgSrc == ""){
+        if(imgSrc.equals("")){
             newsDetail.setImgSrc(null);
         } else {
             //Below line is required to adjust size for mobile
